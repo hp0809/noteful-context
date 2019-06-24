d4413ffa81434405066dd51cff560fa12e9a7e67
Hali Power <hpower0809@gmail.com>
2019-06-24 15:04:47 +0100

    update

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -14,7 +14,6 @@ export default class NoteListMain extends React.Component {
     }
   }
   static contextType = ApiContext
-
   render() {
     const { folderId } = this.props.match.params
     const { notes=[] } = this.context
--- src/NoteListNav/NoteListNav.js
+++ src/NoteListNav/NoteListNav.js
@@ -6,6 +6,7 @@ import ApiContext from '../ApiContext'
 import { countNotesForFolder } from '../notes-helpers'
 import './NoteListNav.css'
 
+
 export default class NoteListNav extends React.Component {
   static contextType = ApiContext;
 
